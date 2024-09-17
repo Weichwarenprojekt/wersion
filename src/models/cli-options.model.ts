1b577290c558102b8bcaaa0f9b54e9d783602374
Tim Niederer <tim.niederer@weichwarenprojekt.de>
2024-09-17 19:48:01 +0200

    feat: add --yes option to skip confirmations

--- src/models/cli-options.model.ts
+++ src/models/cli-options.model.ts
@@ -6,6 +6,8 @@ import { ReleaseType } from "../lib/version";
 export interface CliOptionsModel {
     /** The path the config file. */
     config: string;
+    /** If true, every confirmation is automatically accepted. */
+    yes: boolean;
     /** If true, wersion will not make any git changes. Like that you can test the outcome of wersion. */
     dryRun?: boolean;
     /** Set the release type manually. Creates a new tag and release commit of given type. */
@@ -17,5 +19,6 @@ export interface CliOptionsModel {
  */
 export const defaultCliOptions: CliOptionsModel = {
     config: "./.wersionrc.ts",
+    yes: false,
     dryRun: false,
 };
